bf105f2ebdd8c9c97a55784b6827f08e937f1f93
Paulo Roberto <paulo.pinda@gmail.com>
2024-03-10 19:46:46 -0300

    Custom configurations.

--- run-clickhouse.sh
+++ run-clickhouse.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -p 8123:8123 -p 9000:9000 --name clickhouse-test --ulimit nofile=262144:262144 --rm clickhouse/clickhouse-server
+docker run -d -e CLICKHOUSE_DB=hub -e CLICKHOUSE_USER=admin -e CLICKHOUSE_DEFAULT_ACCESS_MANAGEMENT=1 -e CLICKHOUSE_PASSWORD=admin -p 8123:8123 -p 9000:9000 -p 9009 --name clickhouse-test --ulimit nofile=262144:262144 --rm yandex/clickhouse-server:latest
